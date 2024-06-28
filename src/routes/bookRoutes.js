5e2c4d92bc772dc5b4bc7e1be1b1a7f1d2a0f1a2
kuma <kumaleta2021@gmail.com>
2024-06-28 18:20:11 +0300

    book routes enhanced to handle routes modify review,addReview get book review and delete review

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -3,9 +3,12 @@ const addBook=require('../controllers/bookController/addBook')
 const getBooksAvailable = require("../controllers/bookController/getBooksAvailable");
 const getBooksByAuthor=require('../controllers/bookController/getBooksByAuthor')
 const getBookByISBN=require('../controllers/bookController/getBooksByISBN')
-const getBookReview=require('../controllers/bookController/getBooksByReview')
+const getBookReview=require('../controllers/reviewController/getBookReview')
 const getBooksByTitle = require("../controllers/bookController/getBooksByTitle");
-const protect=require("../middleWare/authenticationMiddleware")
+const protect=require("../middleWare/authenticationMiddleware");
+const addReview = require('../controllers/reviewController/addReview');
+const modifyReview = require('../controllers/reviewController/modifyReview');
+const deleteReview = require("../controllers/reviewController/deleteReview");
 //import for registered user
 
 const router=express.Router()
@@ -13,10 +16,11 @@ router.post('/addBook',protect,addBook)
 //general users 
 router.get('/getAllBooksAvailable',getBooksAvailable)
 router.get('/getBooksByISBN',getBookByISBN)
-router.get('getBooksByAuthor',getBooksByAuthor)
+router.get('/getBooksByAuthor',getBooksByAuthor)
 router.get('/getBooksByTitle',getBooksByTitle)
 router.get('/getBookReview',getBookReview)
 //registered users
-// router.patch('/modifyBookReview',modifyBookReview)
-// router.delete('/deleteBookReviewAdded',deleteBookReviewAdded)
+router.post('/addReview',protect,addReview)
+router.patch('/modifyBookReview',protect,modifyReview)
+router.delete("/deleteReview",protect, deleteReview);
 module.exports= router
