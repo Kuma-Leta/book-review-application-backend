47c90d19349743e23a2d595197ab0b432cb2f676
kuma <kumaleta2021@gmail.com>
2024-06-28 18:15:15 +0300

    get book enhanced to populate reviews in book collection

--- src/controllers/bookController/getBooksAvailable.js
+++ src/controllers/bookController/getBooksAvailable.js
@@ -1,15 +1,20 @@
 const bookModel=require('../../models/bookModel')
 const getBooksAvailable=async(req,res)=>{
 try {
-    const booksAvailable=await bookModel.find()
+    const booksAvailable=await bookModel.find().populate('reviews')
     if(!booksAvailable){
         return res.status(404).json({status:"fail",message:"no books available"})
     }
+    
     res.status(200).json({ status: "success", booksAvailable });
 } catch (error) {
-         return res
-           .status(404)
-           .json({ status: "fail", message: "no books available" ,error:error.message});
+       return res
+  .status(500)
+  .json({
+    status: "fail",
+    message: "internal server Error",
+    error: error.message,
+  }); 
    
 }
 }
